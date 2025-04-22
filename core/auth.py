af25beac4640f597f63b0c19dd065533d243d76b
soulvibescorp <113645673+soulvibescorp@users.noreply.github.com>
2025-04-21 20:44:26 -0400

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -1,1 +1,18 @@
+import json
+import hashlib
+import pyotp
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def create_totp(secret=None):
+    if not secret:
+        secret = pyotp.random_base32()
+    return pyotp.TOTP(secret), secret
+
+def verify_totp(secret, code):
+    totp = pyotp.TOTP(secret)
+    return totp.verify(code)
 
